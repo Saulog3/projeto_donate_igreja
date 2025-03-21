457ea40f3c7cb78fa8a01dcca528aff8e277e47a
Saulo Cunha <saulorg3@gmail.com>
2025-03-21 19:47:33 -0300

    Update progress

---
+++ src/pages/Doacao.js
@@ -0,0 +1,9 @@
+function Doacao() {
+    return (
+        <div>
+            <h1>Cadastro de Doações</h1>
+            <p>Aqui você pode cadastrar novos itens para doação.</p>
+        </div>
+    );
+}
+export default Doacao;
---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+        <div>
+            <h1>Produtos Disponíveis</h1>
+            <p>Lista de produtos para doação e venda.</p>
+        </div>
+    );
+}
+export default Home;
