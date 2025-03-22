18c2d274c1a0969d00a2e2a19e18301cae61216c
Saulo Cunha <saulorg3@gmail.com>
2025-03-22 12:38:57 -0300

    Update

--- src/pages/Doacao.js
+++ src/pages/Doacao.js
@@ -5,9 +5,9 @@ function Doacao() {
         <div>
             <h1>Cadastro de Doações</h1>
             <p>Aqui você pode cadastrar novos itens para doação.</p>
-            <Botao />
+            <Botao link="/" texto="Voltar para a página inicial" />
         </div>
-        
+
     );
 }
 export default Doacao;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import Mensagem from "../components/bem_vindo";
+import Botao from "../components/botao";
 function Home() {
     return (
         <div>
             <h1>Produtos Disponíveis</h1>
             <p>Lista de produtos para doação e venda.</p>
             <Mensagem />
+            <Botao link="/doacao" texto="Cadastrar Doação" />
         </div>
     );
 }
