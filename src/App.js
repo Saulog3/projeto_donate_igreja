54b86a884784d9a3fa188315b65c1d01589805b0
Saulo Cunha <saulorg3@gmail.com>
2025-03-29 22:42:54 -0300

    Update HeroSection

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import HeroSection from "./components/HeroSection";
 import FormularioVaquinha from "./components/FormularioVaquinha";
 import Footer from "./components/Footer";
 import "./styles.css";
+// import Carousel from "./components/Carousel";
+
+
 
 function App() {
     return (
         <div>
             <Navbar />
-            <HeroSection />
+            <div className="conteiner">
+                <HeroSection />
+                {/* <Carousel /> */}
+            </div>
             <FormularioVaquinha />
             <Footer />
         </div>
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,24 +1,20 @@
 import "./HeroSection.css";
 import banner from "../assets/banner.png";
-import Carousel from "./Carousel";
+
 
 
 
 function HeroSection() {
-    const images = [
-        "https://via.placeholder.com/800x400?text=Slide+1",
-        "https://via.placeholder.com/800x400?text=Slide+2",
-        "https://via.placeholder.com/800x400?text=Slide+3",
-      ];
+
     return (
         <section className="hero">
-            <img className="Banner" src={banner} alt="Banner Solidário" />
             <div className="hero-content">
-                <img src="/assets/banner.png" alt="Banner Solidário" className="hero-image"/>
+                <img className="hero-image" src={banner} alt="Banner Solidário" />
+                {/* <img src="../assets/banner.png" alt="Banner Solidário" className="hero-image"/> */}
                 <div className="hero-text">
                     <h1>Para quem é</h1>
                     <p>
-                        Na <strong>Solidário</strong>, transparência e conexões são prioridades...
+                        <strong>Solidário</strong>, transparência e conexões são prioridades...
                     </p>
                     <div className="hero-buttons">
                         <button className="primary">Quero Doar</button>
@@ -26,9 +22,7 @@ function HeroSection() {
                     </div>
                 </div>
             </div>
-            <div className="carousel-container">
-                <Carousel images={images} />
-            </div>
+
         </section>
     );
 }
