6c5f3f15b56cdb97702081ee6907f9cbd4258458
Saulo Cunha <saulorg3@gmail.com>
2025-04-04 20:13:44 -0300

    Finalmente carrossel funcionou (sou burro) update

--- html_css/scripts.js
+++ html_css/scripts.js
@@ -32,22 +32,18 @@ function toggleState3() {
 }
 
 let imgObject = [
-  "https://placeimg.com/450/450/any",
-  "https://placeimg.com/450/450/animals",
-  "https://placeimg.com/450/450/architecture",
-  "https://placeimg.com/450/450/nature",
-  "https://placeimg.com/450/450/people",
-  "https://placeimg.com/450/450/tech",
-  "https://picsum.photos/id/1/450/450",
-  "https://picsum.photos/id/8/450/450",
-  "https://picsum.photos/id/12/450/450",
-  "https://picsum.photos/id/15/450/450",
-  "https://picsum.photos/id/5/450/450",
+  "image/carousel/1.jpeg",
+  "image/carousel/2.jpeg",
+  "image/carousel/3.jpeg",
+  "image/carousel/4.jpeg",
+  "image/carousel/4.jpeg"
 ];
 
 let mainImg = 0;
 let prevImg = imgObject.length - 1;
 let nextImg = 1;
+
+
 
 function loadGallery() {
 
